233c90e4145397d27b8e6f7c9e01d814c2ebe7fa
s1reeen <s1reeen17@gmail.com>
2022-11-27 01:27:28 +0300

    goit-markup-hw-08

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,14 @@
-const refs = {
-  openModalBtn: document.querySelector('[data-open-modal]'),
-  closeModalBtn: document.querySelector('[data-close-modal]'),
-  backdrop: document.querySelector('[data-backdrop]'),
-  modal: document.querySelector('[data-modal]'),
-};
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-refs.openModalBtn.addEventListener('click', tongleModal);
-refs.closeModalBtn.addEventListener('click', tongleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-function tongleModal() {
-  document.body.classList.toggle('modal-open');
-  refs.backdrop.classList.toggle('backdrop--is-hidden');
-  refs.modal.classList.toggle('modal--active');
-}
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
